30f6d9f45e9397e72b2feecbc5b3cf5b06746483
zhuhong.li <xiaohong4512@163.com>
2014-06-10 14:38:45 +0800

    Create utils.js
    add cookie, url, text

---
+++ src/utils.js
@@ -0,0 +1,58 @@
+;(function () {
+    var utils = {
+        cookie: {
+            getCookie: function (key) {
+                var cookie = document.cookie,
+                    reg = new RegExp('(?:^|;\\s*)' + key + '=([^;]+)(?:$|;\\s*)', 'g'),
+                    match = reg.exec(cookie);
+
+                return match === null ? '' : match[1];
+            }
+        },
+        url: {
+            getParam: function (key) {
+                var url = decodeURIComponent(location.href),
+                    reg = new RegExp('(?:[&\\?])(?:' + key + ')=([^#&]+)', 'g'),
+                    match = reg.exec(url);
+
+                return match === null ? '' : match[1];
+            }
+        },
+        text: {
+            getStrLen: function (str) {
+                return String(str).replace(/[^\x00-\xff]/g,"ci").length;
+            },
+            truncateText: function (str, width) {
+                var len = this.getStrLen(str),
+                    ret = '',
+                    i = 0,
+                    l = str.length,
+                    w = 0,
+                    code, c;
+
+                if (len <= width) {
+                    return str;
+                }
+
+                for (; i < l; i++) {
+                    c = str.charAt(i);
+                    code = str.charCodeAt(i);
+
+                    if (code > 255) {
+                        w += 2;
+                    } else {
+                        w += 1;
+                    }
+
+                    if (w < width - 2) {
+                        ret += c;
+                    }
+                }
+
+                return ret + '...';
+            }
+        }
+    };
+
+    window.utils = window.utils || utils;
+})();
